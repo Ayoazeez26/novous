40430fee2db3482c4eea04ae770f54ab1bc68991
Abdulhakeem Abdulazeez <ayoazeez26@gmail.com>
2024-06-23 10:15:40 +0100

    feat: updated design

--- components/Career/Opening.vue
+++ components/Career/Opening.vue
@@ -7,9 +7,129 @@ const openings = ref([
     band: 'MT',
     ageLimit: 'Thirty(30)',
     endDate: '30th May, 2024',
-    link: '#',
+    link: '/career/4',
     img: 'job-place'
-  }
+  },
+  {
+    title: 'Brand/Product Advertising & Media Management Comm Officer',
+    bank: 'Novus Microfinance Bank',
+    location: 'Makurdi, Benue State',
+    band: 'MT',
+    ageLimit: 'Twenty-six(26)',
+    endDate: '30th May, 2024',
+    link: '/career/4',
+    img: 'job-place'
+  },
+  {
+    title: 'Credit Officer',
+    bank: 'Novus Microfinance Bank',
+    location: 'Makurdi, Benue State',
+    band: 'MT2',
+    ageLimit: 'Twenty-five(25)',
+    endDate: '30th May, 2024',
+    link: '/career/4',
+    img: 'credit-place'
+  },
+  {
+    title: 'Head of Operations',
+    bank: 'Novus Microfinance Bank',
+    location: 'Makurdi, Benue State',
+    band: 'Off',
+    ageLimit: 'Thirty-five(35)',
+    endDate: '30th May, 2024',
+    link: '/career/4',
+    img: 'job-place'
+  },
+  {
+    title: 'Teller',
+    bank: 'Novus Microfinance Bank',
+    location: 'Makurdi, Benue State',
+    band: 'MT',
+    ageLimit: 'Twenty-four(24)',
+    endDate: '30th May, 2024',
+    link: '/career/4',
+    img: 'teller-place'
+  },
+  {
+    title: 'Administration & Procurement Officer',
+    bank: 'Novus Microfinance Bank',
+    location: 'Makurdi, Benue State',
+    band: 'MT',
+    ageLimit: 'Thirty(30)',
+    endDate: '30th May, 2024',
+    link: '/career/4',
+    img: 'job-place'
+  },
+  {
+    title: 'Zonal Lead',
+    bank: 'Novus Microfinance Bank',
+    location: 'Makurdi, Benue State',
+    band: 'Officer',
+    ageLimit: 'Thirty-seven(37)',
+    endDate: '30th May, 2024',
+    link: '/career/4',
+    img: 'job-place'
+  },
+  {
+    title: 'Corporate Governance, Regulatory & Legal Compliance Officer ',
+    bank: 'Novus Microfinance Bank',
+    location: 'Makurdi, Benue State',
+    band: 'Off',
+    ageLimit: 'Thirty-five(35)',
+    endDate: '30th May, 2024',
+    link: '/career/4',
+    img: 'job-place'
+  },
+  {
+    title: 'Technology Officer',
+    bank: 'Novus Microfinance Bank',
+    location: 'Makurdi, Benue State',
+    band: 'Off',
+    ageLimit: 'Thirty-five(35)',
+    endDate: '30th May, 2024',
+    link: '/career/4',
+    img: 'job-place'
+  },
+  {
+    title: 'Technical Assistant to The Chief Executive officer ',
+    bank: 'Novus Microfinance Bank',
+    location: 'Makurdi, Benue State',
+    band: 'MT-Officer upon confirmation',
+    ageLimit: 'Thirty-Two(32)',
+    endDate: '30th May, 2024',
+    link: '/career/4',
+    img: 'tech-place'
+  },
+  {
+    title: 'Investment Officer',
+    bank: 'Novus Microfinance Bank',
+    location: 'Makurdi, Benue State',
+    band: 'MT',
+    ageLimit: 'Twenty-seven(27)',
+    endDate: '30th May, 2024',
+    link: '/career/4',
+    img: 'job-place'
+  },
+  {
+    title: 'Collection & Recovery Officer',
+    bank: 'Novus Microfinance Bank',
+    location: 'Makurdi, Benue State',
+    band: 'MT',
+    ageLimit: 'Thirty-three(33)',
+    endDate: '30th May, 2024',
+    link: '/career/4',
+    img: 'job-place'
+  },
+  {
+    title: 'Customer Service Officer',
+    bank: 'Novus Microfinance Bank',
+    location: 'Makurdi, Benue State',
+    band: 'MT',
+    ageLimit: 'Twenty-seven(27)',
+    endDate: '30th May, 2024',
+    link: '/career/4',
+    img: 'service-place'
+  },
 ])
 </script>
 <template>
@@ -37,7 +157,7 @@ const openings = ref([
             <div class="flex items-center w-full gap-4">
               <img
                 class="w-[160px]"
-                src="/img/job-place.webp"
+                :src="`/img/${opening.img}.webp`"
                 alt="job placeholder image"
               />
               <div class="flex flex-col text-grey-10">
@@ -61,7 +181,7 @@ const openings = ref([
             </div>
             <div class="flex flex-col items-end gap-1">
               <p class="min-w-max mb-2">Closing {{ opening.endDate }}</p>
-              <nuxt-link to="/career/4" class="bg-secondary px-8 py-4 rounded-lg text-white">
+              <nuxt-link :to="opening.link" class="bg-secondary px-8 py-4 rounded-lg text-white">
                 Apply Now
               </nuxt-link>
             </div>
